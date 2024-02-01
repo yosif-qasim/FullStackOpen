5703c969140fe63655922480a931e0cf9d081313
yosif-qasim <93684533+yosif-qasim@users.noreply.github.com>
2024-02-02 00:48:00 +0200

    EX 1.7: unicafe step2

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -10,6 +10,10 @@ const App = () => {
     const [neutral, setNeutral] = useState(0)
     const [bad, setBad] = useState(0)
 
+    const total = (good+bad+neutral)
+    const calcAvg = (good ,bad ,neutral) => (good - bad )/(good + bad + neutral)
+    const calcPositive = (good ,bad ,neutral) => ( (good) / (good + bad + neutral) )*100
+
     return (
         <center>
             <Header header="Give Feedback" />
@@ -20,6 +24,9 @@ const App = () => {
             <p> Good {good}</p>
             <p> Neutral {neutral}</p>
             <p> Bad {bad}</p>
+            <p> Total {total}</p>
+            <p> Average {calcAvg(good,bad,neutral)}</p>
+            <p> Positive percent {calcPositive(good,bad,neutral)}% </p>
         </center>
     )
 }
