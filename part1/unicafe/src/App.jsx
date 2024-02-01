25a318ed2391a12d86611eb47be687939c764ee3
yosif-qasim <93684533+yosif-qasim@users.noreply.github.com>
2024-02-02 00:53:10 +0200

    EX 1.8: unicafe step3

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -5,14 +5,28 @@ import { useState } from 'react'
 const Header = ({header}) => <h1>{header}</h1>
 const Button = ({btnName , feedback}) => <button onClick={feedback}>{btnName}</button>
 
+const Stastistics = ({good ,bad ,neutral}) => {
+    const total = (good + bad + neutral)
+    const calcAvg = (good, bad, neutral) => (good - bad) / (good + bad + neutral)
+    const calcPositive = (good ,bad ,neutral) => ( (good) / (good + bad + neutral) )*100
+
+    return(
+        <>
+            <p> Good {good}</p>
+            <p> Neutral {neutral}</p>
+            <p> Bad {bad}</p>
+            <p> Total {total}</p>
+            <p> Average {calcAvg(good, bad, neutral)}</p>
+            <p> Positive percent {calcPositive(good, bad, neutral)}% </p>
+        </>
+    )
+}
 const App = () => {
     const [good, setGood] = useState(0)
     const [neutral, setNeutral] = useState(0)
     const [bad, setBad] = useState(0)
 
-    const total = (good+bad+neutral)
-    const calcAvg = (good ,bad ,neutral) => (good - bad )/(good + bad + neutral)
-    const calcPositive = (good ,bad ,neutral) => ( (good) / (good + bad + neutral) )*100
+
 
     return (
         <center>
@@ -21,12 +35,8 @@ const App = () => {
             <Button btnName={"Neutral"} feedback={()=>setNeutral(neutral +1 )}/>
             <Button btnName={"Bad"} feedback={()=>setBad(bad +1 )}/>
             <Header header="Statistics" />
-            <p> Good {good}</p>
-            <p> Neutral {neutral}</p>
-            <p> Bad {bad}</p>
-            <p> Total {total}</p>
-            <p> Average {calcAvg(good,bad,neutral)}</p>
-            <p> Positive percent {calcPositive(good,bad,neutral)}% </p>
+            <Stastistics good={good} neutral={neutral} bad={bad}/>
+
         </center>
     )
 }
