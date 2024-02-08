d66486b9081cdbb07127e474e961b531496e8aac
yosif-qasim <93684533+yosif-qasim@users.noreply.github.com>
2024-02-08 22:13:59 +0200

    starting part 2

--- part1/anecdotes/src/App.jsx
+++ part1/anecdotes/src/App.jsx
@@ -12,6 +12,7 @@ const App = () => {
         'The only way to go fast, is to go well.'
     ]
 
+
     const [selected, setSelected] = useState(0)
     const [votes, setVotes] = useState(new Uint8Array(anecdotes.length))
 
