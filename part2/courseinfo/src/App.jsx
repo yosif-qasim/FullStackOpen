d1a00dec0e59211792c51a8cd371ce5d5fa2f28e
yosif-qasim <93684533+yosif-qasim@users.noreply.github.com>
2024-02-08 23:35:33 +0200

    EX2.5: Separate module step10

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -1,41 +1,4 @@
-const Course = ({course})=>{
-  return(
-      <>
-        <Header header={course.name} />
-        <Content content={course.parts} />
-        <Total parts={course.parts} />
-      </>
-  )
-}
-const Header = (props) => {
-  return ( <h1>{props.header}</h1> )
-}
-
-const Content = (props) => {
-  return (
-      <>
-        { props.content.map((part) =>
-            <Part key={part.id}
-                  name={part.name}
-                  number={part.exercises}
-            /> )}
-      </>
-  )
-}
-
-const Part = (props) => {
-  return(
-      <p>
-        {props.name} {props.number}
-      </p>
-  )
-}
-const Total = (props) => {
-  const sum = props.parts.reduce( (sum , parts)=>{return sum + parts.exercises} ,0)
-  return (<p> Total of : {sum} exercises</p>)
-}
-
-
+import Course from  "./components/Course";
 const App = () => {
   const courses = [
     {
