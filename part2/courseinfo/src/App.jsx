752dc3151bf10dde8f7fd5020a0fdfb77879ac2b
yosif-qasim <93684533+yosif-qasim@users.noreply.github.com>
2024-02-08 22:44:48 +0200

    EX2.1: Course information step6

---
+++ part2/courseinfo/src/App.jsx
@@ -0,0 +1,63 @@
+const Course = ({course})=>{
+  return(
+      <>
+        <Header header={course.name} />
+        <Content content={course.parts} />
+      </>
+  )
+}
+const Header = (props) => {
+  return ( <h1>{props.header}</h1> )
+}
+
+const Content = (props) => {
+  return (
+      <>
+        { props.content.map((part) =>
+            <Part key={part.id}
+                  name={part.name}
+                  number={part.exercises}
+            /> )}
+      </>
+  )
+}
+
+const Part = (props) => {
+  return(
+        <p>
+          {props.name} {props.number}
+        </p>
+  )
+}
+
+
+
+const App = () => {
+  const course = {
+    id: 1,
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      }
+    ]
+  }
+
+  return <Course course={course} />
+}
+
+
+
+export default App
